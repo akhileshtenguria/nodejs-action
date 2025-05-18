6304f39da077eef619ca97d7e72b1c6eea470103
akhileshtecnotree <143830632+akhileshtecnotree@users.noreply.github.com>
2025-05-18 17:50:56 +0530

    changes push

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
-import userRoutes from './routes/userRoutes.js';
-import mongodb from './config.js';
+// import userRoutes from './routes/userRoutes.js';
+// import mongodb from './config.js';
 import dotenv  from 'dotenv';
 dotenv.config();
 
-mongodb();
+// mongodb();
 const PORT= process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
@@ -18,7 +18,7 @@ app.get('/:title', (req,res)=>{
     res.render("home", {title:title});
 })
 
-app.use('/v1/api/', userRoutes);
+// app.use('/v1/api/', userRoutes);
 
 
 
